5631fc9667bde0a84e0cc99e363c651c10c05c39
fedeghe <fedeghe@gmail.com>
2019-02-03 16:58:28 +0100

    small step forward

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# typeHero
+# typehero
---
+++ source/lib/index.js
@@ -0,0 +1,6 @@
+(function () {
+    $$../../node_modules/@fedeghe/leonardo/dist/index.js$$
+    $$../../node_modules/@fedeghe/channeljs/dist/index.js$$
+    $$../../LICENSE.md$$
+    $$sensor.js$$
+})();
