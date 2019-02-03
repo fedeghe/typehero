b4864b1b22bd101fbf462653b242b73ef873d139
fedeghe <fedeghe@gmail.com>
2019-02-03 20:58:03 +0100

    basic stuff added ...cound be enough

--- source/lib/index.js
+++ source/lib/index.js
@@ -1,6 +1,8 @@
 (function () {
     $$../../node_modules/@fedeghe/leonardo/dist/index.js$$
     $$../../node_modules/@fedeghe/channeljs/dist/index.js$$
-    $$../../LICENSE.md$$
+    $$../../node_modules/balle/dist/index.js$$
+    $$../../node_modules/ridof/dist/index.js$$
+    $$../../node_modules/widgzard/dist/index.js$$
     $$sensor.js$$
 })();
