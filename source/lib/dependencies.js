9b5b22d8474e0a0774de2953d6196474dcc186b6
fedeghe <fedeghe@gmail.com>
2019-02-04 18:27:39 +0100

    separate deps

---
+++ source/lib/dependencies.js
@@ -0,0 +1,5 @@
+$$../../node_modules/@fedeghe/leonardo/dist/index.js$$
+$$../../node_modules/@fedeghe/channeljs/dist/index.js$$
+$$../../node_modules/balle/dist/index.js$$
+$$../../node_modules/ridof/dist/index.js$$
+$$../../node_modules/widgzard/dist/index.js$$
--- source/lib/index.js
+++ source/lib/index.js
@@ -1,8 +1,4 @@
 (function () {
-    $$../../node_modules/@fedeghe/leonardo/dist/index.js$$
-    $$../../node_modules/@fedeghe/channeljs/dist/index.js$$
-    $$../../node_modules/balle/dist/index.js$$
-    $$../../node_modules/ridof/dist/index.js$$
-    $$../../node_modules/widgzard/dist/index.js$$
+    $$dependencies.js$$
     $$sensor.js$$
 })();
