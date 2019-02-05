733d58103a142b17b0df7bc184f0b6811ec29bad
fedeghe <fedeghe@gmail.com>
2019-02-06 00:39:09 +0100

    start somewhere

--- source/lib/ui.js
+++ source/lib/ui.js
@@ -2,6 +2,9 @@ var trg = document.getElementById('trg'),
     app = Widgzard.render({
         target: trg,
         content: [{
-            html: 'lorem'
+            tag: 'textarea',
+            attrs: {
+                class: 'big'
+            }
         }]
     });
